a26e3545c8fac8eff16812cf17d6b2904bb43986
Folusho Adeyemi <folushoadeyemiv@meta.com>
2023-06-14 11:21:31 -0700

    Added button and event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Helllo")
+
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent form submission (optional)
+
+    // Perform your desired action here
+    console.log("Button clicked!");
+});
